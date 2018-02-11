177337c5515227e66f4b639aa3ad449f591c8469
KuzminKirill <wee3dood@gmail.com>
2018-02-11 23:01:10 +0300

    it's allive

---
+++ app/src/main/java/com/example/kirill/retrofittry/Activities/App.java
@@ -0,0 +1,31 @@
+package com.example.kirill.retrofittry.Activities;
+
+import android.app.Application;
+
+import com.example.kirill.retrofittry.Interfaces.API;
+import com.example.kirill.retrofittry.Settings.Settings;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+
+public class App extends Application {
+    private static API api;
+    private Retrofit retrofit;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        retrofit = new Retrofit.Builder()
+                .baseUrl(Settings.POINT_URL) //Базовая часть адреса
+                .addConverterFactory(GsonConverterFactory.create()) //Конвертер, необходимый для преобразования JSON'а в объекты
+                .build();
+        api = retrofit.create(API.class); //Создаем объект, при помощи которого будем выполнять запросы
+    }
+
+    public static API getApi() {
+        return api;
+    }
+}
+
