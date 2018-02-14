b2323a4cb956c5c8ee8fa290adcf12f21d985e18
KuzminKirill <wee3dood@gmail.com>
2018-02-15 01:59:24 +0300

    add app and some changes

--- app/src/main/java/com/example/kirill/retrofittry/Activities/App.java
+++ app/src/main/java/com/example/kirill/retrofittry/Activities/App.java
@@ -3,6 +3,7 @@ package com.example.kirill.retrofittry.Activities;
 import android.app.Application;
 
 import com.example.kirill.retrofittry.Interfaces.API;
+import com.example.kirill.retrofittry.Interfaces.GetCourses;
 import com.example.kirill.retrofittry.Settings.Settings;
 
 import retrofit2.Retrofit;
@@ -10,6 +11,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class App extends Application {
     private static API api;
+    private static GetCourses allcourses;
     private Retrofit retrofit;
 
     @Override
@@ -20,11 +22,15 @@ public class App extends Application {
                 .baseUrl(Settings.POINT_URL) //Базовая часть адреса
                 .addConverterFactory(GsonConverterFactory.create()) //Конвертер, необходимый для преобразования JSON'а в объекты
                 .build();
-        api = retrofit.create(API.class); //Создаем объект, при помощи которого будем выполнять запросы
+        api = retrofit.create(API.class);//Создаем объект, при помощи которого будем выполнять запросы
+        allcourses = retrofit.create(GetCourses.class);
     }
 
     public static API getApi() {
         return api;
+    }
+    public static GetCourses getCourses() {
+        return allcourses;
     }
 }
 
--- app/src/main/java/com/example/kirill/retrofittry/Interfaces/GetCourses.java
+++ app/src/main/java/com/example/kirill/retrofittry/Interfaces/GetCourses.java
@@ -3,13 +3,15 @@ package com.example.kirill.retrofittry.Interfaces;
 import com.example.kirill.retrofittry.Parsers.Course;
 import com.example.kirill.retrofittry.Parsers.Courses;
 
+import java.util.List;
+
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
 
 public interface GetCourses {
     @GET("courses")
-    Call<Courses> all();
+    Call<List<Course>> all();
 
     @GET("courses/{id}")
     Call<Course> select(@Path("id") int id);
